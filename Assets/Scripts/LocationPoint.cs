4e170cb5c2afb0ec82804cf1cf92eb7e104be3e8
ArturCodeAcademy <123232198+ArturCodeAcademy@users.noreply.github.com>
2023-01-28 12:23:21 +0200

    2_2 pamoka

---
+++ Assets/Scripts/LocationPoint.cs
@@ -0,0 +1,19 @@
+using TMPro;
+using UnityEngine;
+
+public class LocationPoint : MonoBehaviour
+{
+    public string Text { set => _text.text = value; }
+
+    [SerializeField] private TMP_Text _text;
+
+    void Start()
+    {
+        UpdateForward();
+    }
+
+    public void UpdateForward()
+    {
+        transform.forward = transform.position.normalized;
+    }
+}
